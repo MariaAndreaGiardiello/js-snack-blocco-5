44bf40d2d26d9df5660f7bc5fd9704cec04b726b
MariaAndreaGiardiello <giardiello.m.a@icloud.com>
2022-05-19 16:53:25 +0200

    genrato array zucchine

--- snack-1/js/main.js
+++ snack-1/js/main.js
@@ -1,2 +1,60 @@
 //Crea un array di 10 oggetti che rappresentano una zucchina, indicando per ognuna varietà, peso e lunghezza.
 //Calcola quanto pesano tutte le zucchine
+
+// 1. creo array con zucchine
+const zucchine = [
+    {
+        nome: "Zucchina 1",
+        peso: 10,
+        lunghezza: 20,
+    },
+    {
+        nome: "Zucchina 2",
+        peso: 13,
+        lunghezza: 8,
+    },
+    {
+        nome: "Zucchina 3",
+        peso: 20,
+        lunghezza: 10,
+    },
+    {
+        nome: "Zucchina 4",
+        peso: 23,
+        lunghezza: 6,
+    },
+    {
+        nome: "Zucchina 5",
+        peso: 17,
+        lunghezza: 5,
+    },
+    {
+        nome: "Zucchina 6",
+        peso: 18,
+        lunghezza: 17,
+    },
+    {
+        nome: "Zucchina 7",
+        peso: 20,
+        lunghezza: 23,
+    },
+    {
+        nome: "Zucchina 8",
+        peso: 18,
+        lunghezza: 9,
+    },
+    {
+        nome: "Zucchina 9",
+        peso: 10,
+        lunghezza: 10,
+    },
+    {
+        nome: "Zucchina 10",
+        peso: 16,
+        lunghezza: 20,
+    }
+]
+
+// 2. creo costante per contenere il totale del peso
+// 3. creo ciclo for per continuare a sommare fino a che non comprende tutto il peso delle 10 zucchine
+// 4. faccio la somma del peso all'interno del ciclo
