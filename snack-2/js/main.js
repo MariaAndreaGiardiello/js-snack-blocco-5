6188233f285694db8b5b23e78049d06941ed3b5c
MariaAndreaGiardiello <giardiello.m.a@icloud.com>
2022-05-19 17:09:21 +0200

    inizo snack-2

---
+++ snack-2/js/main.js
@@ -0,0 +1,68 @@
+//Crea 10 oggetti che rappresentano una zucchina.
+//Dividi in due array separati le zucchine che misurano meno o più di 15cm.
+//Infine stampa separatamente quanto pesano i due gruppi di zucchine.
+
+// 1. creo array con 10 zucchine
+const zucchine = [
+    {
+        nome: "Zucchina 1",
+        peso: 10,
+        lunghezza: 20,
+    },
+    {
+        nome: "Zucchina 2",
+        peso: 13,
+        lunghezza: 8,
+    },
+    {
+        nome: "Zucchina 3",
+        peso: 20,
+        lunghezza: 10,
+    },
+    {
+        nome: "Zucchina 4",
+        peso: 23,
+        lunghezza: 6,
+    },
+    {
+        nome: "Zucchina 5",
+        peso: 17,
+        lunghezza: 5,
+    },
+    {
+        nome: "Zucchina 6",
+        peso: 18,
+        lunghezza: 17,
+    },
+    {
+        nome: "Zucchina 7",
+        peso: 20,
+        lunghezza: 23,
+    },
+    {
+        nome: "Zucchina 8",
+        peso: 18,
+        lunghezza: 9,
+    },
+    {
+        nome: "Zucchina 9",
+        peso: 10,
+        lunghezza: 10,
+    },
+    {
+        nome: "Zucchina 10",
+        peso: 16,
+        lunghezza: 20,
+    }
+]
+
+// 2. creo due array per contenere zucchine < o > di 15 cm
+const lunghezzaMag = [];
+const lunghezzaMin = [];
+
+// 3. creo variabile per generare la il totale del peso
+let totale = 0;
+
+// 4. creo ciclo for per comprendere tutte le lunghezze indicate all'intenro dell'array di zucchine
+//5. all'intenro del ciclo for creo un ciclo if per dividere le due lunghezze
+// 6. creo ciclo for per la somma
